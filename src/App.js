0b54c22a5a0464049c5b38e31801170d59992b9c
Hermes Echavarria <hermese@gmail.com>
2023-01-08 19:38:56 -0500

    navbar; page state, Products component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { Header } from './components/Header';
-import { Main } from './components/Main';
-import './App.css';
-import { Footer } from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
+import { Header } from './components/Header'
+import { Main } from './components/Main'
+import './App.css'
+import { Footer } from './components/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [page, setPage] = useState('inicio')
+
   return (
     <div>
-      <Header/>
-      <Main/>
+      <Header setPage={setPage}/>
+      <Main page={page}/>
       <Footer/>
     </div>
   );
--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -6,11 +6,8 @@ export function ProductForm() {
     
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control type="text" placeholder="Ingrese nombre" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
